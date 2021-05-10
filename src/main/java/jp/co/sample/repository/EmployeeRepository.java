8b30feca51684130a814eed8028c4a903237ca79
natsumifukuhara <natsumi.fukuhara@rakus-partners.co.jp>
2021-05-10 14:19:53 +0900

    管理者登録画面が表示されました

---
+++ src/main/java/jp/co/sample/repository/EmployeeRepository.java
@@ -0,0 +1,63 @@
+package jp.co.sample.repository;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.stereotype.Repository;
+
+import jp.co.sample.domain.Employee;
+
+@Repository
+public class EmployeeRepository {
+	
+	@Autowired
+	private NamedParameterJdbcTemplate template;
+	
+	private static final RowMapper<Employee>EMPLOYEE_ROW_MAPPER = (rs,i) ->
+	{
+		Employee employee = new Employee();
+		
+		employee.setId(rs.getInt("id"));
+		employee.setName(rs.getString("name"));
+		employee.setImage(rs.getString("image"));
+		employee.setGender(rs.getString("gender"));
+		employee.setHireDate(rs.getDate("hire_date"));
+		employee.setMailAddress(rs.getString("mail_address"));
+		employee.setZipCode(rs.getString("zip_code"));
+		employee.setAddress(rs.getString("address"));
+		employee.setTelephone(rs.getString("telephone"));
+		employee.setSalary(rs.getInt("salary"));
+		employee.setCharacteristics(rs.getString("characteristics"));
+		employee.setDependentsCount(rs.getInt("depemdemts_count"));
+		return employee;
+	};
+	
+	//従業員一覧情報を取得する（入社日順）
+	public List<Employee> findAll(){
+		String sql="SELECT * FROM employees ORDER BY hire_date";
+		List<Employee> employeeList=template.query(sql, EMPLOYEE_ROW_MAPPER);
+		return employeeList;
+	}
+	
+	//主キーから従業員情報を取得する
+	public Employee load(Integer id) {
+		String sql="SELECT * FROM employees WHERE id=:id";
+		SqlParameterSource param= new MapSqlParameterSource().addValue("id", id);
+		Employee employee = template.queryForObject(sql, param, EMPLOYEE_ROW_MAPPER);
+		return employee;
+		
+		
+	}
+	//従業員情報を更新する
+	public void update(Employee employee) {
+		SqlParameterSource param = new BeanPropertySqlParameterSource(employee);
+		String sql="UPDATE employees SET dependents_count=:dependentsCount WhERE id=:id";
+		template.update(sql, param);		
+	}
+	
+}
